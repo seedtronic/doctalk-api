bdc2579b7555c7ecc5bcf702f4722fa0b56ff768
marano <thiagomarano@gmail.com>
2017-10-28 21:46:41 -0200

    Add id field to doctor type.

--- app/graphql/types/doctor_type.rb
+++ app/graphql/types/doctor_type.rb
@@ -2,6 +2,7 @@ Types::DoctorType = GraphQL::ObjectType.define do
   name 'Doctor'
   description 'A Doctor'
 
+  field :id, !types.ID
   field :name, !types.String
   field :lat, types.Float
   field :lng, types.Float
