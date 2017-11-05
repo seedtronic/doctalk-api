2ed4db6f92a8b0919dfc4e1935eacfc576ca0e76
marano <thiagomarano@gmail.com>
2017-11-04 23:48:02 -0200

    Endpoint for getting doctor by id.

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -2,6 +2,11 @@ Types::QueryType = GraphQL::ObjectType.define do
   name 'Query'
   description 'The root query'
 
+  field :doctor, Types::DoctorType do
+    argument :id, types.ID
+    resolve ->(obj, args, ctx) { Doctor.find(args[:id]) }
+  end
+
   connection :doctors, Types::DoctorType.connection_type do
     description 'List doctors'
     resolve ->(obj, args, ctx) { Doctor.all }
