f428911ff2f5d73db14210ac6a9f713c046e6751
marano <thiagomarano@gmail.com>
2017-11-25 20:27:08 -0200

    Add name and specialty do doctor input.

--- app/graphql/inputs/doctor_input.rb
+++ app/graphql/inputs/doctor_input.rb
@@ -2,5 +2,7 @@ Inputs::DoctorInput = GraphQL::InputObjectType.define do
   name 'DoctorInput'
   description 'A Doctor'
 
+  input_field :name, !types.String
+  input_field :specialty_id, !types.ID
   input_field :address, !Inputs::AddressInput
 end
