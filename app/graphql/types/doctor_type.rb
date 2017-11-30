050664f115630e92383e29d8fa2b250167888a1a
marano <thiagomarano@gmail.com>
2017-11-30 21:57:15 -0200

    Add crm field to doctor type.

--- app/graphql/types/doctor_type.rb
+++ app/graphql/types/doctor_type.rb
@@ -5,6 +5,7 @@ Types::DoctorType = GraphQL::ObjectType.define do
   field :id, !types.ID
   field :name, !types.String
   field :imageUrl, types.String, property: :image_url
+  field :crm, !types.String
   field :specialty, Types::SpecialtyType
   field :address, Types::AddressType
   field :user, Types::UserType
